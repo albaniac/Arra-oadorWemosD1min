0deaa60c6236735c230daf27ff7f1af595e8f586
tbagro <tiagounemat@gmail.com>
2017-07-07 11:46:36 -0400

    Add files via upload
    inserem o acionamento do OTA e a função de controle dos pinos de forma gerenerica

---
+++ runanyway.h
@@ -0,0 +1,9 @@
+//aciona os pinos definidos na função
+void runAndWait(int pin, int Status, int interval) // pino, status, invervalo
+{
+  digitalWrite(pin,  Status);
+  delay(interval);
+  digitalWrite(pin,  !Status);
+  delay(100);
+}
+
