e120ae3598c2043737751653b5739677e50c1014
tbagro <tiagounemat@gmail.com>
2018-01-25 09:48:18 -0300

    Update eeprom.h

--- eeprom.h
+++ eeprom.h
@@ -6,11 +6,11 @@ uint8_t addr1 = 7;    // status_auto
 uint8_t eeAddress;
 
 // Funções para gerenciamento
-
-void Save_Data() {
+// A função Save_Data foi montada de forma a evitar o desgaste da memoria EEPROM Salvando apenas os valores se o mesmo for alterado
+void Save_Data() {// adicione essa função onde os dados são alterados
  EEPROM.put(addr1,  status_auto);
   eeAddress = 9;    //endereço inicial eeprom
-  EEPROM.put(eeAddress , tempoD1);      // Grava a variavel se o valor for diferente
+  EEPROM.put(eeAddress , tempoD1);      // Grava a variavel somente se o valor for diferente do anterior
   eeAddress += sizeof(int);              // Incrementa o endereco com valor do tamanho da variavel gravada (int).
   EEPROM.put(eeAddress , tempoD2);      // Grava a variavel se o valor for diferente
   eeAddress += sizeof(int);             // Incrementa o endereco com valor do tamanho da variavel gravada (int).
@@ -20,7 +20,7 @@ void Save_Data() {
   Serial.println("*WifiRTC: Dados salvos na EEPROM");
 }
 
-void Read_Data() {
+void Read_Data() {// incluida no void Eeprom()
  eeAddress = 9; // recupera o endereço
   EEPROM.get(addr1,  status_auto);//
   EEPROM.get(eeAddress , tempoD1);    // Le a EEPROM e salva na variavel
@@ -41,7 +41,7 @@ void Eeprom() { //colocar no setup
   delay(250);
 }
 
-void clearEeprom() {
+void clearEeprom() {// função para limpar a memoria eeprom 
   for (int i = 0; i <= 512; i++) {
     EEPROM.write(i, 0);
     EEPROM.end();
